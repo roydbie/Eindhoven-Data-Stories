714e1c9f702441cb052e6b5d76f490cb71402d20
roydebie <48086834+roydbie@users.noreply.github.com>
2023-04-04 00:43:11 +0200

    Added: card setup for later

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,7 +1,19 @@
 import ListGroup from "./components/ListGroup";
+import "./App.css";
 
 function App() {
-    return <div><ListGroup></ListGroup></div>;
+  return (
+    <>
+      <div className="container-xl text-center">
+        <div className="topic">Dit wordt dan een card</div>
+        <div className="topic">.g-col-6</div>
+        <div className="topic">.g-col-6</div>
+        <div className="topic">.g-col-6</div>
+      </div>
+      <br></br>
+      <ListGroup></ListGroup>
+    </>
+  );
 }
 
 export default App;
--- frontend/src/components/ListGroup.tsx
+++ frontend/src/components/ListGroup.tsx
@@ -1,12 +1,29 @@
+import { useState } from "react";
+
 function ListGroup() {
+  let items = ["New York", "San Fransisco", "Haaren", "Tilburg"];
+  const [selectedIndex, setSelectedIndex] = useState(-1);
+
   return (
-    <ul className="list-group">
-      <li className="list-group-item">An item</li>
-      <li className="list-group-item">A second item</li>
-      <li className="list-group-item">A third item</li>
-      <li className="list-group-item">A fourth item</li>
-      <li className="list-group-item">And a fifth one</li>
-    </ul>
+    <>
+      <h1>List example</h1>
+      {items.length === 0 && <p>No items found.</p>}
+      <ul className="list-group">
+        {items.map((item, index) => (
+          <li
+            className={
+              selectedIndex === index
+                ? "list-group-item active"
+                : "list-group-item"
+            }
+            key={item}
+            onClick={() => setSelectedIndex(index)}
+          >
+            {item}
+          </li>
+        ))}
+      </ul>
+    </>
   );
 }
 
