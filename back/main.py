78092f26249a06e5f9aeb3e67cf02a5464561509
roydebie <48086834+roydbie@users.noreply.github.com>
2023-04-05 18:02:39 +0200

    Added: custom urls for backend

--- back/main.py
+++ back/main.py
@@ -3,9 +3,14 @@ import json
 
 app = Flask(__name__)
 
-@app.route('/', methods=['GET'])
-def index():
+@app.route('/publicreports', methods=['GET'])
+def publicreports():
     data = {'data': [10, 11, 12, 13, 14]}
+    return json.dumps(data)
+
+@app.route('/neighbourhoods', methods=['GET'])
+def neighbourhoods():
+    data = {'data': 'neighbourhoods'}
     return json.dumps(data)
 
 if __name__ == '__main__':
