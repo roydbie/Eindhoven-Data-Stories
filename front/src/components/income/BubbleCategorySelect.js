2b966bade6636ac701bb9df4dc8f2a0380ae7dc3
roydebie <48086834+roydbie@users.noreply.github.com>
2023-05-09 13:45:32 +0200

    Test

--- front/src/components/income/BubbleCategorySelect.js
+++ front/src/components/income/BubbleCategorySelect.js
@@ -66,7 +66,7 @@ function BubbleCategorySelect() {
         <div className="col-10">
           <BubbleChart category={category} xAxis={xAxis}></BubbleChart>
         </div>
-        <div className="col-2">Hallo?</div>
+        <div className="col-2">Hssssallo?</div>
       </div>
     </>
   );
