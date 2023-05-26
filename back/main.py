26885fa5de41fa42f56e7c8ec627088842aa2006
irisofferman <irisofferman@hotmail.com>
2023-05-26 15:43:48 +0300

    Added data_clean and calculating IQR

--- back/main.py
+++ back/main.py
@@ -1,25 +1,30 @@
 import pandas as pd
-import numpy as np 
-
+import numpy as np
 
 # Read the CSV file
-df = pd.read_csv('data.csv', delimiter=";", decimal=",")
+df = pd.read_csv('data_clean.csv')
 
-column_mapping = {
-    'Buurten': 'neighbourhood',
-    'Gemiddeld persoonlijk inkomen per inkomensontvanger (x1000 euro)|2020': 'personal_income',
-    'Inwoners|2020': 'residents',
-    'Voelt zich niet zo gelukkig of ongelukkig %|2020': 'unhappy_percentage',
-    'Door langdurige ziekte sterk belemmerd thuis %|2020': 'prolonged_illness_percentage',
-    'Hoog opleidingsniveau %|2020': 'high_level_of_education_percentage',
-    'Maakt u zich zorgen om geld %|2020': 'worries_about_money_percentage',
-    'Heeft schulden %|2020': 'has_debts_percentage',
-    '65 jaar en ouder %|2020': '65plus_percentage'
-}
-df.rename(columns=column_mapping, inplace=True)
-
-# Print the updated column names
 print(df)
+
+#Calculate Q1, Q3, and IQR
+Q1 = df.quantile(0.25)
+Q3 = df.quantile(0.75)
+
+print(Q1)
+print(Q3)
+
+IQR = Q3 - Q1
+
+#print(IQR)
+
+#set threshold at 1.5
+threshold = 1.5
+
+#determine outliers
+outliers = (df < Q1 - threshold * IQR) | (df > Q3 + threshold * IQR)
+
+
+
 
 
 
